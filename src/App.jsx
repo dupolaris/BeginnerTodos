348dc670a2a8e725e474dd927f9e195aff45b177
dawood <dupolaris.dev@gmail.com>
2024-11-13 07:50:23 -0800

    user can input new todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
+import { useState } from "react";
+
 import { Header } from "./components/Header";
 import { Tabs } from "./components/Tabs";
 import { TodoList } from "./components/TodoList";
 import { TodoInput } from "./components/TodoInput";
 
 function App() {
-  const todos = [
-    { input: "Hello, add new code", complete: true },
-    { input: "Go to gym", complete: true },
-    { input: "Learn React", complete: false },
-    { input: "Get a coffee", complete: false },
-    { input: "Get some cert", complete: true },
-  ];
+  // const todos = [
+  //   { input: "Hello, add new code", complete: true },
+  //   { input: "Go to gym", complete: true },
+  //   { input: "Learn React", complete: false },
+  //   { input: "Get a coffee", complete: false },
+  //   { input: "Get some cert", complete: true },
+  // ];
+
+  const [todos, setTodos] = useState([
+    { input: "Hello, add new To-do", complete: false },
+  ]);
+
+  function handleTodoInput(newTodo) {
+    const newTodoList = [{ input: newTodo, complete: false }, ...todos];
+    setTodos(newTodoList);
+  }
 
   return (
     <>
       <Header todos={todos} />
       <Tabs todos={todos} />
       <TodoList todos={todos} />
-      <TodoInput />
+      <TodoInput handleTodoInput={handleTodoInput} />
     </>
   );
 }
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,8 +1,30 @@
-export function TodoInput() {
+import { useState } from "react";
+
+export function TodoInput(props) {
+  const { handleTodoInput } = props;
+
+  const [inputValue, setInputValue] = useState("");
+
+  //console.log(inputValue);
+
   return (
     <div>
-      <input placeholder="Add your task" />
-      <button>
+      <input
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+        placeholder="Add your task"
+      />
+      <button
+        onClick={() => {
+          if (!inputValue) {
+            return;
+          }
+          handleTodoInput(inputValue);
+          setInputValue("");
+        }}
+      >
         <span className="text-2xl">+</span>
       </button>
     </div>
